527e17edd4c1c48b53982468b68711ab9459e5bb
NateFrogHI <n.k.mitson@gmail.com>
2021-03-18 22:13:15 -0700

    Full commit

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    [SerializeField] AudioClip breakSound;
+
+    // Cashed reference
+    Level level;
+    GameStatus gameStatus;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        gameStatus = FindObjectOfType<GameStatus>();
+        level.CountBreakableBlocks();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
+    {
+        gameStatus.AddToScore();
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        level.BlockDestroyed();
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameStatus : MonoBehaviour
+{
+    // configuration parameters
+    [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
+    [SerializeField] int pointsPerBlockDestroyed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    // state variables
+    [SerializeField] int currentScore = 0;
+
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+
+        if (gameStatusCount > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        updateScoreText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+
+    public void AddToScore()
+    {
+        currentScore += pointsPerBlockDestroyed;
+        updateScoreText();
+    }
+
+    public void updateScoreText()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+}
