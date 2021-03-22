816e54fe8a6eb0314d761da98e0b0ba520da2ba3
NateFrogHI <n.k.mitson@gmail.com>
2021-03-21 23:03:12 -0700

    Added block type tags

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -13,14 +13,25 @@ public class Block : MonoBehaviour
 
     private void Start()
     {
+        CountBreakableBlocks();
+    }
+
+    private void CountBreakableBlocks()
+    {
         level = FindObjectOfType<Level>();
         gameStatus = FindObjectOfType<GameSession>();
-        level.CountBreakableBlocks();
+        if (tag == "Breakable")
+        {
+            level.CountBlocks();
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        DestroyBlock();
+        if (tag == "Breakable")
+        {
+            DestroyBlock();
+        }
     }
 
     private void DestroyBlock()
